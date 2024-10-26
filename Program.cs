615e936ec1c55232bc937d5babbcf7c407007b8b
kaliel <kalielsoares6@gmail.com>
2024-10-26 17:56:16 -0300

    Alteração no codigo

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
-﻿using System.IO;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Xml.Linq;
-public class person
+﻿public class person
 {
     public required string name { get; set; }
     public required int age { get; set; }
@@ -17,29 +13,27 @@ class program
         //cria a lista
         List<person> people = new List<person>();
 
-        do
+        Console.WriteLine("Vamos cadastrar uma pessoa?");
+        string answer = Console.ReadLine();
+
+        while (answer != "n")
         {
+            Console.WriteLine("Qual o nome dessa pessoa?");
+            string Name = Console.ReadLine();
+            Console.WriteLine("e qual seria a sua idade?");
+            int Age = int.Parse(Console.ReadLine());
+            Console.WriteLine("ela trabalha com o que?");
+            string Profission = Console.ReadLine();
+            Console.WriteLine("e o genero dela?");
+            char Gender = char.Parse(Console.ReadLine());
 
-            Console.WriteLine("Vamos cadastrar uma pessoa?");
-            string answer = Console.ReadLine();
+            people.Add(new person { name = Name, age = Age, profission = Profission, gender = Gender });
 
-            if (answer == "n")
-            {
-                break;
-            }
-            else
-            {
-                string Name = Console.ReadLine();
-                int Age = int.Parse(Console.ReadLine());
-                string Profission = Console.ReadLine();
-                char Gender = char.Parse(Console.ReadLine());
-
-                people.Add(new person { name = Name, age = Age, profission = Profission, gender = Gender });
-
-            }
-
+            Console.WriteLine("Vamos cadastrar uma nova pessoa?");
+            answer = Console.ReadLine();
         }
-        while (true);
+
+
 
         foreach (person Person in people)
         {
